5af890f7350bc08db3cb0f2119415e3192f1594e
rodlmoraes <rodlmoraes@hotmail.com>
2020-09-20 18:15:01 -0300

    add create-lesson-service base service some configs

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,13 @@ Bundler.require(*Rails.groups)
 
 module EsiProject
   class Application < Rails::Application
+    config.eager_load_paths << Rails.root.join('lib')
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers

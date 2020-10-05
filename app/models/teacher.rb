5410999134bb454bdf375373f4554d0cd4b0816b
rodlmoraes <rodlmoraes@hotmail.com>
2020-10-05 17:10:14 -0300

    remove devise :trackable from teacher

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Teacher < ApplicationRecord
-  devise :database_authenticatable, :registerable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :validatable
   include DeviseTokenAuth::Concerns::User
   extend Devise::Models
   has_many :lessons, dependent: :destroy

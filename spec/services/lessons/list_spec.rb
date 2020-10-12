e54129ffab4f6d077996422807374a6bbf541aa1
João Francisco Cocca Fukuda <joao.fukuda@protonmail.com>
2020-10-12 15:50:02 +0000

    refac(test): arrumar casos teste
    Arrumar para suportar a query no `GET` da API do lessons/index

--- spec/services/lessons/list_spec.rb
+++ spec/services/lessons/list_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Lessons::List, type: :service do
-  let(:response) { described_class.call }
+  let(:query) {""}
+  let(:response) { described_class.call({query: query}) }
 
   let(:created_lessons_size) { 5 }
   let(:teacher) { create(:teacher) }
